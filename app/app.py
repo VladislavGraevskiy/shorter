a405f249981ba2f1fc649fa73e8e8aafb6c63dd7
Vladislav Graevskiy <v.graevskiy@admitad.com>
2018-12-19 23:21:43 +0300

    init

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-From flask import Flask
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,5 +6,5 @@ def hello_world:
     return 'Не грусти!!!'
 
 
-if __name == '__main__':
+if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
